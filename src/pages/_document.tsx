2335ae1a12343f6d77fc6fc993560ddc459f7f5b
Joseph Kalayci <44485937+josephkalayci@users.noreply.github.com>
2022-10-07 19:21:48 -0400

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,46 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="english">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=DM Serif Display"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat Subrayada"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://kajoo.nyc3.digitaloceanspaces.com/kajoo-assets/fonts/sfprotext.css"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=DM Serif Display"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat Subrayada"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=DM Serif Display"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat Subrayada"
+          ></link>
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript></NextScript>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default CustomDocument
