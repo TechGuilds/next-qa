0196ff80ade023fd3d27df0d18a2ebe3659a3fdb
moh-akteeb <moh-alkteeb@gmail.com>
2022-10-14 00:29:23 +0300

    sdebugging Link  component

--- src/components/CompLink.tsx
+++ src/components/CompLink.tsx
@@ -10,18 +10,44 @@ import styles from './CompLink.module.css'
 const CompLink = (props) => {
   return (
     <Container className={styles['root']}>
+      <ErrorBoundary param={props}>
       <Container
         className={` ${styles['container']} ${styles['container-default']} `}
-      >
+        >
         <Link
           href={props.fields.href}
           className={` ${styles['link']} ${styles['link-link-style-2']} `}
-        />
+          />
       </Container>
+  </ErrorBoundary>
     </Container>
   )
 }
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log("CompLinkError",error, errorInfo,props?.param);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
 CompLink.defaultProps = {
   fields: {},
 }
--- src/components/StaticText.tsx
+++ src/components/StaticText.tsx
@@ -10,14 +10,41 @@ import styles from './StaticText.module.css'
 const StaticText = (props) => {
   return (
     <Container className={styles['root']}>
+   <ErrorBoundary param={props}>
+
       <Link
         href={props.fields.href}
         className={` ${styles['link']} ${styles['link-link-style-2']} `}
       />
+      </ErrorBoundary>
     </Container>
   )
 }
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log("StaticTextError",error, errorInfo,props?.param);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
 StaticText.defaultProps = {
   fields: {},
 }
