35370d5e87990140e37eb5c2bda9b673720f4bdc
yusuf <yusuf.kalayci@techguilds.com>
2022-10-17 06:55:39 -0400

    add error boundry to CollectionOfLinks

--- src/components/CollectionOfLinks.tsx
+++ src/components/CollectionOfLinks.tsx
@@ -1,19 +1,26 @@
 /* eslint-disable */
 // @ts-nocheck
-import React from 'react'
-import { Container, Link, Button } from 'kajoo-components/sitecore-nextjs'
+import React from 'react';
+import { Container, Link, Button } from 'kajoo-components/sitecore-nextjs';
 
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-import styles from './CollectionOfLinks.module.css'
+import styles from './CollectionOfLinks.module.css';
 
-const CollectionOfLinks = (props) => {
+const CollectionOfLinks = (props = {}) => {
+  console.log('CollectionOfLinks', { props });
+  return (
+    <ErrorBoundary>
+      <MyComp {...props} />
+    </ErrorBoundary>
+  );
+};
+
+const MyComp = (props) => {
   return (
     <Container className={` ${styles['root']} ${styles[props.rootClassName]} `}>
       <Container className={styles['container']}>
-        <Container
-          className={` ${styles['container1']} ${styles['container-default']} `}
-        >
+        <Container className={` ${styles['container1']} ${styles['container-default']} `}>
           <Link
             href={props.fields.href1}
             className={` ${styles['link']} ${styles['link-link-style-2']} `}
@@ -28,9 +35,7 @@ const CollectionOfLinks = (props) => {
             className={` ${styles['link2']} ${styles['link-link-style-2']} `}
           />
         </Container>
-        <Container
-          className={` ${styles['container2']} ${styles['container-default']} `}
-        >
+        <Container className={` ${styles['container2']} ${styles['container-default']} `}>
           <Button
             text="Button"
             link="Linnk"
@@ -43,17 +48,43 @@ const CollectionOfLinks = (props) => {
         </Container>
       </Container>
     </Container>
-  )
+  );
+};
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
 }
 
 CollectionOfLinks.defaultProps = {
   rootClassName: '',
   fields: {},
-}
+};
 
 CollectionOfLinks.propTypes = {
   rootClassName: PropTypes.string,
   fields: PropTypes.object,
-}
+};
 
-export default CollectionOfLinks
+export default CollectionOfLinks;
