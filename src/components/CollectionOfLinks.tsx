6c2dd0dbfd6e2f42637e58762ac0db6eea103c2f
yusuf <yusuf.kalayci@techguilds.com>
2022-10-17 10:08:33 -0400

    link test

--- src/components/CollectionOfLinks.tsx
+++ src/components/CollectionOfLinks.tsx
@@ -1,28 +1,39 @@
 /* eslint-disable */
 // @ts-nocheck
-import React, { useState, useEffect } from "react";
-import { Container, Link, Button } from 'kajoo-components/sitecore-nextjs';
-
-import PropTypes from 'prop-types';
-
+import React, { useState, useEffect } from 'react';
+import { Container, Link } from 'kajoo-components/sitecore-nextjs';
+import { Link as SitecoreLink } from '@sitecore-jss/sitecore-jss-nextjs';
 import styles from './CollectionOfLinks.module.css';
 
-const CollectionOfLinks = (props) => {
-  if (!props.fields?.href1?.value?.href || !props.fields?.href3?.value?.href) {
-    return (
-      <div>
-        href missing {JSON.stringify({ href1: props.fields?.href1, href3: props.fields?.href3 })}
-      </div>
-    );
-  }
+const Link: FC<any> = (props) => {
+  const { href = '', text = '', ...rest } = props;
+  const textField = typeof text === 'string' ? { value: text } : text;
+  const field = typeof href === 'string' ? { value: { href, text: textField.value } } : href;
 
-  if (!props.fields?.text1?.value || !props.fields?.text4?.value) {
-    return (
-      <div>
-        text missing {JSON.stringify({ text1: props.fields?.text1, text4: props.fields?.text4 })}
-      </div>
-    );
-  }
+  useEffect(() => {
+    console.log('link', { props, textField, field });
+  }, [props, field, textField]);
+
+  return <div>hello link</div>;
+};
+export default Link;
+
+const CollectionOfLinks = (props) => {
+  // if (!props.fields?.href1?.value?.href || !props.fields?.href3?.value?.href) {
+  //   return (
+  //     <div>
+  //       href missing {JSON.stringify({ href1: props.fields?.href1, href3: props.fields?.href3 })}
+  //     </div>
+  //   );
+  // }
+
+  // if (!props.fields?.text1?.value || !props.fields?.text4?.value) {
+  //   return (
+  //     <div>
+  //       text missing {JSON.stringify({ text1: props.fields?.text1, text4: props.fields?.text4 })}
+  //     </div>
+  //   );
+  // }
 
   return (
     <Container className={` ${styles['root']} ${styles[props.rootClassName]} `}>
@@ -43,7 +54,7 @@ const CollectionOfLinks = (props) => {
           />
         </Container>
         <Container className={` ${styles['container2']} ${styles['container-default']} `}>
-          <Button
+          {/* <Button
             text="Button"
             link="Linnk"
             className={` ${styles['button']} ${styles['button-button-364']} `}
@@ -51,21 +62,11 @@ const CollectionOfLinks = (props) => {
           <Button
             text={props.fields.text4}
             className={` ${styles['button1']} ${styles['button-button-540']} `}
-          />
+          /> */}
         </Container>
       </Container>
     </Container>
   );
-};
-
-CollectionOfLinks.defaultProps = {
-  rootClassName: '',
-  fields: {},
-};
-
-CollectionOfLinks.propTypes = {
-  rootClassName: PropTypes.string,
-  fields: PropTypes.object,
 };
 
 export default CollectionOfLinks;
