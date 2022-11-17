2c56e6421c5130968de05335771e3fb88d01017a
Jehad <jehad.alkhateeb@techguilds.com>
2022-11-16 20:01:06 -0500

    correcting fields

--- src/components/EventCardComp.tsx
+++ src/components/EventCardComp.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable */
 // @ts-nocheck
-import React from 'react'
-import { Container, Text } from 'kajoo-components/sitecore-nextjs'
+import React from 'react';
+import { Container, Text } from 'kajoo-components/sitecore-nextjs';
 
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-import styles from './EventCardComp.module.css'
+import styles from './EventCardComp.module.css';
 
 const AppComponent = (props) => {
   return (
@@ -13,25 +13,26 @@ const AppComponent = (props) => {
       <Container className={styles['container']}>
         <Container className={styles['container1']}>
           <Container className={styles['container2']}>
-            <Text text={props.Header} className={styles['text']} />
-            <Text text={props.Desc} className={styles['text1']} />
+            <Text text={props.fields.Header} className={styles['text']} />
+            <Text text={props.fields.Desc} className={styles['text1']} />
           </Container>
         </Container>
       </Container>
     </Container>
-  )
-}
+  );
+};
 
 AppComponent.defaultProps = {
   rootClassName: '',
   Header: 'Thank you!',
-  Desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-}
+  Desc:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+};
 
 AppComponent.propTypes = {
   rootClassName: PropTypes.string,
   Header: PropTypes.string,
   Desc: PropTypes.string,
-}
+};
 
-export default AppComponent
+export default AppComponent;
